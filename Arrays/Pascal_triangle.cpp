f68937e110d64b1a5d6225dc5c45d7ac3035aa74
Shubham Rana <shubhamrana132@gmail.com>
2018-06-26 17:40:17 +0530

    updated on 26th june

--- Arrays/Pascal_triangle.cpp
+++ Arrays/Pascal_triangle.cpp
@@ -1,3 +1,16 @@
+/*
+	--- Question Statement ---
+	
+	Pascal’s triangle is a triangular array of the binomial coefficients. Write a function that takes an integer value n 
+	as input and prints first n lines of the Pascal‟s triangle. Following are the first 6 rows of Pascal‟s Triangle.           
+	1
+	1 1
+	1 2 1
+	1 3 3 1
+	1 4 6 4 1
+	1 5 10 10 5 1 
+*/
+
 #include <iostream>
 #include <string.h>
 #include <vector>
