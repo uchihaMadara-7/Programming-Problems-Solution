f9202fb84b0c875d9264d7d6451343646622b106
Shubham Rana <shubhamrana132@gmail.com>
2018-06-26 22:06:47 +0530

    Updated on 26th june

--- Arrays/one_number_occur_odd_number_of_times.cpp
+++ Arrays/one_number_occur_odd_number_of_times.cpp
@@ -1,3 +1,11 @@
+/*
+	--- Question Statement ---
+	
+	Given an array with every element appears even number of times, and only one element appears odd number of times.
+	Find the number that appears odd number of times.
+	Example: [1,2,2,3,3] output: 1
+*/
+
 #include <iostream>
 
 using namespace std;
