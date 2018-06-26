0cc83859c845b97d8a0aa7f486c3c27c2a98e387
Shubham Rana <shubhamrana132@gmail.com>
2018-06-26 22:10:45 +0530

    updated on 16th june

--- Arrays/pairFactors.cpp
+++ Arrays/pairFactors.cpp
@@ -1,3 +1,11 @@
+/*
+	--- Question Statement ---
+	
+	Given a number n, print all the factors of the number in pairs under square root complexity.
+	Example : n = 16
+	output : (1,16) (2,8) (4,4)
+*/
+
 #include <iostream>
 #include <cmath>
 
