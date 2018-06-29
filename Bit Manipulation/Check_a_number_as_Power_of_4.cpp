788a1d2f6b1f85fbea0f3b1cc909c67412f1218f
Shubham Rana <shubhamrana132@gmail.com>
2018-06-29 12:00:33 +0530

    updated on 29th june

--- Bit Manipulation/Check_a_number_as_Power_of_4.cpp
+++ Bit Manipulation/Check_a_number_as_Power_of_4.cpp
@@ -1,20 +1,43 @@
+/*
+	--- Question Statement ---
+	
+	Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+	Example : n = 16
+	output: true
+*/
+
 #include <iostream>
 
 using namespace std;
 
-bool isPowerOf4(int n)
+bool isPowerOfFour(int num) 
 {
-	int bits = 8 * sizeof(int);
-	int count = 0;
+	if(num < 0)
+	    return false;
 
-	for(int i=0;i<bits;i+=2)
+	int bits = sizeof(int) * 8;
+	int last = 0;
+
+	int count = 0;
+	for(int i=0;i<bits-1;i++)
 	{
-		if((n & 1 << i) && !(n & 1 << (i+1)))
-			count ++;
+	    if(num & 1 << i)
+	    {
+		count ++;
+		last = i;
+	    }
 	}
 
-	if(count == 1)
-		return true;
+	int count1 = 0;
+	for(int i=0;i<last;i++)
+	{
+	    if(!(num & 1 << i))
+		count1 ++;
+	}
+
+	cout << count << " " << count1 << endl;
+	if(count == 1 && count1 % 2 == 0)
+	    return true;
 	return false;
 }
 
