c30d625521bf0f8eac3e1b73fa7ae78a69505fe7
Shubham Rana <shubhamrana132@gmail.com>
2018-07-03 09:11:42 +0530

    updated on 3rd july

--- Arrays/pair_difference_equal_k.cpp
+++ Arrays/pair_difference_equal_k.cpp
@@ -1,3 +1,12 @@
+/*
+	--- Question Statement ---
+	
+	Find the total number of pairs whose sum equals to a given number k.
+	Example: array = [1 2 3 4 5 6]   k = 7
+	output: 3 
+	Explanation: (1,6) (2,5) (3,4)
+*/
+
 #include <iostream>
 #include <cmath>
 
